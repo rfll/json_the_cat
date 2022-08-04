fa232c0d1f328c009f4c375a23deb6e0877568be
Ryan Fortin <ryanwfortin@gmail.com>
2022-08-04 22:06:12 +0000

    Removed console statements from function and used callback in conditions

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,40 +1,33 @@
 const request = require('request');
 
-// // Get breed from command line arg
-// const breedArg = process.argv[2];
-
-
-
-
 const fetchBreedDescription = function(breedName, callback) {
 
   // URL with command line arg in breed search
   const URL = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
   
-// Request function
-request(URL, (error, response, body) => {
+  // Request function
+  request(URL, (error, response, body) => {
   
-  // Log error to console
-  if (error) {
+    // Log error to console
+    if (error) {
+      callback(error, null);
+      return;
+    }
 
-    callback(error, null);
-    return;
-  }
+    // JSON data to object
+    const data = JSON.parse(body);
+    // Access object with breeds
+    const breed = data[0];
 
-  // JSON data to object
-  const data = JSON.parse(body);
-  // Access object with breeds
-  const breed = data[0];
-
-  // String log if the command line breed doesn't exist
-  if (!breed) {
-    console.log(`${breedName} not found.`);
-  // If breed is found, log the description
-  } else {
-    console.log(data[0].description);
-  }
-  // console.log(typeof data);
-});
+    // If the command line breed doesn't exist
+    if (!breed) {
+      callback(null, `${breedName} not found.`);
+    } else {
+      // If breed is found, log the description
+      callback(null, data[0].description);
+    }
+    // console.log(typeof data);
+  });
 };
 
 
