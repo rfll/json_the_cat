166dd9620b078cb19416d349d3ac8f422f92968a
Ryan Fortin <ryanwfortin@gmail.com>
2022-08-04 22:44:56 +0000

    Ran eslint

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const { fetchBreedDescription } = require('./breedFetcher');
 
 // Get breed from command line arg
-const breedArg = process.argv[2];
+const breedName = process.argv[2];
 
-fetchBreedDescription(breedArg, (error, desc) => {
+fetchBreedDescription(breedName, (error, desc) => {
   if (error) {
     console.log('Error fetch details:', error);
   } else {
     console.log(desc);
   }
 });
-
-fetchBreedDescription;
